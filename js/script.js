cb6891fe7d20ff7650a9eec8db4d5119190cbb9f
mustafa-wp <mu6afa23@gmail.com>
2025-08-16 23:53:48 +0300

    first commit12

--- js/script.js
+++ js/script.js
@@ -23,3 +23,45 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 });
+
+
+/**********Products************/
+
+    async function fetchProducts() {
+      try {
+        const res = await fetch("https://fakestoreapi.com/products");
+        const products = await res.json();
+        const productsDiv = document.getElementById("products");
+
+        products.forEach((product) => {
+          const col = document.createElement("div");
+          col.className = "col-md-3 mb-4";
+          col.innerHTML = `
+            <div class="product-card border p-3 rounded">
+              <div class="text-center">
+                <img src="${product.image}" alt="${product.title}" class="img-fluid" style="height:150px;object-fit:contain;">
+                <h6 class="mt-2">${product.title}</h6>
+                <p><strong>$${product.price}</strong></p>
+              </div>
+              <div class="actions mt-3 d-flex justify-content-between">
+                <button class="btn btn-success add-to-cart" data-id="${product.id}">Add to Cart</button>
+                <button class="btn-heart btn btn-outline-danger"><i class="fa-regular fa-heart"></i></button>
+              </div>
+            </div>
+          `;
+          productsDiv.appendChild(col);
+        });
+      } catch (err) {
+        console.error("Error fetching products:", err);
+      }
+    }
+
+    fetchProducts();
+
+    // Event delegation: يطبع الـ id في الكونسول
+    document.addEventListener("click", function(e) {
+      if (e.target.classList.contains("add-to-cart")) {
+        const productId = e.target.getAttribute("data-id");
+        console.log(productId);
+      }
+    });
